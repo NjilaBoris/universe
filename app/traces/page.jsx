0e6cf4a33db8e83907ef29a274f6b9e43fccf0df
NjilaBoris <njilaboris2@gmail.com>
2026-01-14 19:02:17 +0100

    Implement Traces Page

---
+++ app/traces/page.jsx
@@ -0,0 +1,14 @@
+import CardReveal from "@/components/CardReveal/CardReveal";
+import Footer from "@/components/Footer/Footer";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <CardReveal />
+      <Footer />
+    </>
+  );
+};
+
+export default page;
